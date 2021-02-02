f2e1d987d1b9a62c389d65f8f0c782570d3e9932
unknown <sweeptic@gmail.com>
2021-02-02 12:29:17 +0100

    anagrams

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -1,5 +1,5 @@
 // --- Directions
-// Check to see if two provided strings are anagrams of eachother.
+// Check to see if two provided strings are anagrams of each other.
 // One string is an anagram of another if it uses the same characters
 // in the same quantity. Only consider characters, not spaces
 // or punctuation.  Consider capital letters to be the same as lower case
@@ -8,6 +8,27 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function matcher(str) {
+  return (
+    str
+      .trim()
+      .toLowerCase()
+      .replace(/[^\w]/g, '')
+      .split('')
+      //  .filter(item => /[A-Za-z]/.test(item))
+      .sort((a, b) => {
+        if (a < b) return -1;
+        if (a > b) return 1;
+        return 0;
+      })
+      .join('')
+  );
+}
 
-module.exports = anagrams;
+function anagrams(stringA, stringB) {
+  return matcher(stringA) === matcher(stringB) ? true : false;
+}
+
+console.log(anagrams('rail safety', 'fairy tales')); //-- > True;
+console.log(anagrams('RAIL! SAFETY!', 'fairy tales')); //-- > True;
+console.log(anagrams('Hi there', 'Bye there')); //-- > False;
