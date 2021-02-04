b01f66b8e0655111cb994fd0c1200fac81852db8
unknown <sweeptic@gmail.com>
2021-02-04 14:37:13 +0100

    capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,28 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  const res = str
+    .split(' ')
+    .map(item => item[0].toUpperCase() + item.slice(1))
+    .join(' ');
 
-module.exports = capitalize;
+  console.log(res);
+}
+
+capitalize('hi there, how is it going?');
+capitalize('i love breakfast at bill miller bbq');
+
+/*
+test('capitalizes the first letter of every word in a sentence', () => {
+   expect(capitalize('hi there, how is it going?')).toEqual(
+     'Hi There, How Is It Going?'
+   );
+ });
+ 
+ test('capitalizes the first letter', () => {
+   expect(capitalize('i love breakfast at bill miller bbq')).toEqual(
+     'I Love Breakfast At Bill Miller Bbq'
+   );
+ });
+ */
